f196cb7927e421b17969b6cd0c983331204bc04f
anthony.goddard <anthony@fppnt.com>
2019-06-26 11:05:08 +0100

    Initial Commit

---
+++ SolverBenchmarking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SolverBenchmarking
+//
+//  Created by AnthonyFPP on 26/06/2019.
+//  Copyright © 2019 AnthonyFPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
