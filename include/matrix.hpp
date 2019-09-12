857b299d02585adf1a5c35f448857888a72dea0b
anthonyjamesgoddard <antg3254@gmail.com>
2019-09-12 08:55:30 +0100

    using cmake to avoid (yes, avoid) headaches

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,18 +1,17 @@
 #ifndef MATRIXDEF
 #define MATRIXDEF
 #include <cmath>
-
+#include <iostream>
 // File : matrix.h
 // Abstract base class for deriving matrices
 // that are to be used on non-embedded devices
 // i.e your Macbook
 
 namespace SolvAnt {
-namespace Matrix {
+namespace Base {
+template<typename T, std::size_t Rows, std::size_t Columns>
 class Matrix {
 private:
-    int m_Rows;
-    int m_Cols;
 public:
     Matrix();
     virtual ~Matrix();
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "/include/banded_matrix.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    BandedMatrix<double,10,2> r; 
     return 0;
 }
