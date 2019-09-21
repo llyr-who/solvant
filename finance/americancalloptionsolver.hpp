816e0e8537c249e355c111e3be7151d145fa122f
anthonyjamesgoddard <antg3254@gmail.com>
2019-09-21 10:03:43 +0100

    updated american option

--- finance/americancalloptionsolver.hpp
+++ finance/americancalloptionsolver.hpp
@@ -1,14 +1,16 @@
 #ifndef AMERICALL
 #define AMERICALL
 
+#include "solver/tripsor.hpp"
+
 namespace solvant {
 namespace finance {
 template <typename T>
 class AmericanCallOptionSolver {
 public:
-    EuroCallOptionSolver();
-    void init(float E, float T, float sig, float r,
-              solvant::solver::IterativeTriSolver<float, N - 2>* solver);
+    AmericanCallOptionSolver();
+    void init(float E, float T, float sig, float r, maxIterations,
+              solvant::solver::TriPSOR<float, N - 2>* solver);
     void solve();
     void writeSolutionToFile(std::string filename);
 
@@ -16,21 +18,13 @@ private:
     bool m_init;  // has the solver been init'd?
 
     // Financial variables
-    float m_E;    // Exercise price
-    float m_T;    // Expiry date
     float m_sig;  // Volitility
     float m_r;    // interest rate
 
-    // non-dimensional parameters
-    float m_k;
-
     float m_dt;
     float m_dx;
-    float m_alpha;
 
-
-    solvant::solver::IterativeTriSolver<float, N - 2>* p_solver;
-    // pImpl -< can use differnt direct trisolver
+    solvant::solver::TriPSOR<float, N - 2>* p_solver;
     std::array<std::array<float, N - 2>, M> m_sols;
 };
 }  // namespace finance
