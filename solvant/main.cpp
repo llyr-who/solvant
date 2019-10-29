f925bbe2d5a9ef7e38dffb03fc984922a719c021
FPPGod <anthony@fppnt.com>
2019-10-29 14:21:19 +0000

    Revert "cleaning things up a bit"
    This reverts commit 7681a822d4f957da48d7ec929afde6ac19d12a4d.

--- solvant/main.cpp
+++ solvant/main.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
 #include "base/matrix.hpp"
-#include "utils/timer.hpp"
 int main(int argc, const char* argv[]) {
-    solvant::utils::timer t;
-	solvant::base::matrix<double, 800,800> A;
-	solvant::base::matrix<double, 800,800> B;
-    solvant::base::matrix<double, 800,800> C;
-    for(int i = 0; i < 800; i++) {
-        for(int j = 0; j < 800; j++) {
-            A(i,j) = i;
-            B(i,j) = j;
-        }
-    }
-    auto begin = t.elapsed();
-    solvant::base::matrix_prod(A,B,C);
-    auto end = t.elapsed();
-    std::cout << end - begin << std::endl;
+	solvant::base::matrix<double, 10,10> A;
+	solvant::base::matrix<double, 10,10> B;
+	auto C = A*B;
 }
