926c32aea337e39c6cefbbf69262c26ad0cfd13d
llyr-who <antg3254@gmail.com>
2019-10-29 14:32:13 +0000

    updating matrix multiplication

--- base/matrix.hpp
+++ base/matrix.hpp
@@ -1,5 +1,6 @@
 #ifndef MATRIXDEF
 #define MATRIXDEF
+#include <array>
 #include <cmath>
 #include <iostream>
 #include <memory>
@@ -8,51 +9,44 @@ namespace base {
 template <typename T, std::size_t R, std::size_t C>
 class matrix {
 private:
-	std::array<T, R*C> m_data;
+    std::array<T, R * C> m_data;
+
 public:
-	matrix() {};
-	virtual ~matrix() {};
-    
-    std::size_t rows() const { return R;}
+    matrix(){};
+    virtual ~matrix(){};
+
+    std::size_t rows() const { return R; }
     std::size_t cols() const { return C; }
 
     void print() {
         for (std::size_t i = 0; i < R; i++) {
             std::cout << '\n';
             for (std::size_t j = 0; j < C; j++) {
-                std::cout << (*this)(i,j) << " ";
+                std::cout << (*this)(i, j) << " ";
             }
             std::cout << '\n';
         }
         std::cout << std::endl;
     }
 
-    // safety - when implementing, check bounds
-    //virtual T at(const std::size_t i, const std::size_t j) = 0;
-    //virtual void set(const std::size_t i, const std::size_t j, const T val) = 0;
-    
-    // no safety
     virtual T operator()(const std::size_t i, const std::size_t j) const {
-    	return m_data[i*C + j];
+        return m_data[i * C + j];
     }
+
     virtual T& operator()(const std::size_t i, const std::size_t j) {
-    	return m_data[i*C + j];
+        return m_data[i * C + j];
     }
-
-    template<size_t K>
-    matrix<T,R, K> operator*(const matrix<T, C, K>& rhs) const {
-    	matrix<T,R,K> result;
-    	for(int i = 0; i < R; i++) {
-    		for(int j = 0; j < K;j++){
-    			for(int k = 0; k < C; k++) {
-    				result(i,j) += (*this)(i,k) * rhs(k,j);
-    			}
-    		}
-    	}
-    	return result;
-    }
-
+    
+    T* operator[](const std::size_t row) { return &m_data[row * C]; }
 };
+
+template<typename T, std::size_t R, std::size_t K, std::size_t C>
+inline void matrix_prod(const matrix<T, R, K>& a,
+                        const matrix<T, K, C>& b,
+                        matrix<T, R, C>& c) {
+    // implement matrix multiplication
+}
+
 }  // namespace base
 }  // namespace solvant
 #endif
--- solvant/main.cpp
+++ solvant/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "base/matrix.hpp"
+#include "utils/timer.hpp"
 int main(int argc, const char* argv[]) {
-	solvant::base::matrix<double, 10,10> A;
-	solvant::base::matrix<double, 10,10> B;
-	auto C = A*B;
+    solvant::utils::timer t;
+	solvant::base::matrix<double, 800,800> A;
+	solvant::base::matrix<double, 800,800> B;
+    solvant::base::matrix<double, 800,800> C;
+    for(int i = 0; i < 800; i++) {
+        for(int j = 0; j < 800; j++) {
+            A(i,j) = i;
+            B(i,j) = j;
+        }
+    }
+    auto begin = t.elapsed();
+    solvant::base::matrix_prod(A,B,C);
+    auto end = t.elapsed();
+    std::cout << end - begin << std::endl;
 }
