d85a94d94bef755616d744237c4b00ca4bc82c94
llyr-who <antg3254@gmail.com>
2019-11-02 17:18:19 +0000

    optimised multiplication

--- base/matrix.hpp
+++ base/matrix.hpp
@@ -1,11 +1,11 @@
 #ifndef MATRIXDEF
 #define MATRIXDEF
-#include <array>
 #include <cmath>
 #include <iostream>
 #include <memory>
 #include <type_traits>
 #include <utility>
+#include <vector>
 namespace solvant {
 namespace base {
 
@@ -18,35 +18,23 @@ using all_same_type = std::conjunction<std::is_same<T, Ts>...>;
 template <typename T, std::size_t R, std::size_t C>
 class matrix {
 private:
-    std::array<T, R * C> m_data;
+    std::vector<T> m_data;
 
 public:
-    constexpr matrix(){};
+    constexpr matrix() { m_data.resize(R * C); };
 
     //! matrix a = {1,2,3,...}
     template <typename... Ts>
     constexpr matrix(Ts&&... elements) noexcept {
         static_assert(all_same_type<T, Ts...>::value, "Types do not match");
-        static_assert(sizeof...(Ts) == R * C, "Size of array does not match");
-        m_data = std::array<T, R * C>{std::forward<Ts>(elements)...};
+        static_assert(sizeof...(Ts) == R * C, "Size of vector does not match");
+        m_data = std::vector<T>{std::forward<Ts>(elements)...};
     }
 
     ~matrix(){};
 
     std::size_t rows() const { return R; }
     std::size_t cols() const { return C; }
-
-    // move outside class, make inline
-    void print() {
-        for (std::size_t i = 0; i < R; i++) {
-            std::cout << '\n';
-            for (std::size_t j = 0; j < C; j++) {
-                std::cout << (*this)(i, j) << " ";
-            }
-            std::cout << '\n';
-        }
-        std::cout << std::endl;
-    }
 
     T operator()(const std::size_t i, const std::size_t j) const {
         return m_data[i * C + j];
@@ -68,20 +56,32 @@ public:
     }
 };
 
+template<typename T, std::size_t R, std::size_t C>
+inline void print(const matrix<T, R,C> &a) {
+    for (std::size_t i = 0; i < R; i++) {
+        std::cout << '\n';
+        for (std::size_t j = 0; j < C; j++) {
+            std::cout << a(i, j) << " ";
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
+
 /** Calculates the product between two matrices.
  */
 template <typename T, std::size_t R, std::size_t K, std::size_t C>
 inline void matrix_prod(const matrix<T, R, K>& a, const matrix<T, K, C>& b,
-                        matrix<T, R, C>& c) {
+                          matrix<T, R, C>& c) {
     for (std::size_t i = 0; i < R; i++) {
         const auto ai = a[i];  // obtain row i of a
         auto ci = c[i];        // obtain row i of c
-        for (std::size_t j = 0; j < C; j++) {
-            T r = 0;
-            for (size_t k = 0; k < K; k++) {
-                r += ai[k] * b[k][j];
+        for (std::size_t k = 0; k < K; k++) {
+            const auto bk = b[k];    // obtain row k of b
+            const auto aik = ai[k];  // obtain raw data - cir. mul. indices
+            for (std::size_t j = 0; j < C; j++) {
+                ci[j] += aik * bk[j];
             }
-            ci[j] = r;
         }
     }
 }
