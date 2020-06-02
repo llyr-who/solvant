1ea3749abe9120bb7fb1fb402b34add552a17b90
llyr-who <jamesbushnell1992@gmail.com>
2020-06-02 13:36:27 +0100

    updated to fetchContent

--- solvant/utils/timer.hpp
+++ solvant/utils/timer.hpp
@@ -2,18 +2,18 @@
 #include <chrono>
 
 namespace solvant {
-namespace utils {
 class timer {
 public:
     timer() : m_beg(m_clock::now()) {}
     void reset() { m_beg = m_clock::now(); }
     double elapsed() const {
-        return std::chrono::duration_cast<m_second>(m_clock::now() - m_beg).count();
+        return std::chrono::duration_cast<m_second>(m_clock::now() - m_beg)
+            .count();
     }
+
 private:
     typedef std::chrono::high_resolution_clock m_clock;
     typedef std::chrono::duration<double, std::ratio<1>> m_second;
     std::chrono::time_point<m_clock> m_beg;
 };
-}  // namespace utils
 }  // namespace solvant
