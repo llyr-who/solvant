63f2cb43f1e25bb4897e5f7990cfef6fcb66b192
llyr-who <antg3254@gmail.com>
2020-06-07 14:37:03 +0100

    attempt to fix test

--- solvant/algo/ge.hpp
+++ solvant/algo/ge.hpp
@@ -2,15 +2,17 @@
 #include "core/matrix.hpp"
 #include "core/vector.hpp"
 
+#include <memory>
+
 namespace solvant {
 
 template <typename T, size_t N>
-matrix<T, N, N>& identity() {
+std::unique_ptr<matrix<T, N, N>> identity() {
     matrix<T, N, N> I; 
-    for(int i; i < N; i++){
+    for(int i = 0; i < N; i++){
         I[i][i] = 1;
     }
-    return I;
+    return std::make_unique<matrix<T,N,N>>(I);
 }
 
 template <typename T, std::size_t N>
@@ -21,8 +23,8 @@ void ge(const solvant::vector<T, N>& rhs, const solvant::matrix<T, N, N>& m,
     // This will solve the matrix using guasian ellimination with partial
     // pivoting
     matrix U = m;
-    matrix L = identity<T,N>();
-    matrix P = identity<T,N>();
+    matrix L = *identity<T,N>();
+    matrix P = *identity<T,N>();
 
     for (int k = 0; k < N - 1; k++) {
         // select i to max u{ik}
@@ -49,23 +51,23 @@ void ge(const solvant::vector<T, N>& rhs, const solvant::matrix<T, N, N>& m,
     vector<T, N> y;
 
     // Call forward substitution (returns vector) to solve "Ly=bb"
-    y(0) = bb(0) / L(0, 0);
+    y[0] = bb[0] / *L[0, 0];
     for (int j = 1; j < N; j++) {
         double SUM = 0;
         for (int k = 0; k < j; k++) {
-            SUM += y(k) * L(j, k);
+            SUM += y[k] * *L[j, k];
         }
-        y(j) = (bb(j) - SUM) / L(j, j);
+        y[j] = (bb[j] - SUM) / *L[j, j];
     }
 
     // Call backward substitution Ux = y
-    x(N - 1) = y(N - 1) / U(N - 1, N - 1);
+    x[N - 1] = y[N - 1] / *U[N - 1, N - 1];
     for (int j = N - 1; j > -1; j--) {
         double SUM = 0;
         for (int k = j + 1; k < N; k++) {
-            SUM += x(k) * U(j, k);
+            SUM += x[k] * *U[j, k];
         }
-        x(j) = (y(j) - SUM) / U(j, j);
+        x[j] = (y[j] - SUM) / *U[j, j];
     }
 }
 
--- solvant/algo/tests/ge_test.cpp
+++ solvant/algo/tests/ge_test.cpp
@@ -1,8 +1,9 @@
 #include "algo/ge.hpp"
 
+#include <iostream>
+
 #include "core/matrix.hpp"
 #include "core/vector.hpp"
-
 #include "gtest/gtest.h"
 
 template <std::size_t N>
@@ -11,15 +12,22 @@ using sq_matrix = solvant::matrix<double, N, N>;
 template <std::size_t N>
 using vector = solvant::vector<double, N>;
 
-TEST(ge_test, basic_solv){
+TEST(ge_test, basic_solv) {
     sq_matrix<2> m = {1.0, 1.0, 3.0, -2.0};
-    vector<2> v = {-9.0, 4.0};
+    vector<2> v = {3.0, 4.0};
 
-    vector<2> soln = {-5.0,-5.0};
+    vector<2> soln = {2.0, 1.0};
 
     vector<2> test;
 
-    solvant::ge(v, m, soln);
+    solvant::ge(v, m, test);
 
-    ASSERT_TRUE(test == soln);
+    std::cout << test[1] << std::endl;
+    std::cerr << "[          ] test = " << "(" << test[0]<< "," << test[1] << ")" << std::endl;
+    
+
+    ASSERT_TRUE(test[0] == soln[0]);
+    ASSERT_TRUE(test[1] == soln[1]);
+
+    //ASSERT_TRUE(test == soln);
 }
